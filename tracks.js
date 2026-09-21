32f5870927af8ac04b10bec38dd3a36c37f60a5d
Alex McVittie <alexander.mcvittie@gmail.com>
2026-09-21 19:07:32 -0400

    add to portfolio

--- tracks.js
+++ tracks.js
@@ -8,7 +8,8 @@ const hierarchy = {
     "genresBroad": {
       "Rock": [
         "Alternative Rock",
-        "Classic Rock"
+        "Classic Rock",
+        "Progressive Rock"
       ],
       "Funk & Soul": [
         "Funk",
@@ -46,18 +47,46 @@ const hierarchy = {
 
 const bassTracks = [
   {
-    "title": "Demo Artist - Placeholder Groove",
-    "file": "audio/demo-bass.wav",
-    "artwork": "img/portfolio/placeholder-1.svg",
+    "title": "Arcane - Eternity",
+    "file": "audio/Arcane_-_Eternity.mp3",
+    "artwork": "img/portfolio/arcane-sorry-for-the-mess.webp",
     "genresBroad": ["Funk & Soul"],
-    "genres": ["Funk"]
+    "genres": ["R&B"]
   },
   {
-    "title": "Demo Artist - Placeholder Rocker",
-    "file": "audio/demo-bass.wav",
-    "artwork": "img/portfolio/placeholder-2.svg",
+    "title": "Arcane - Pretend Lovers",
+    "file": "audio/Arcane_-_Pretend_Lovers.mp3",
+    "artwork": "img/portfolio/arcane-pretendlovers.webp",
+    "genresBroad": ["Funk & Soul"],
+    "genres": ["R&B"]
+  },
+  {
+    "title": "Galanthus - A False Hope",
+    "file": "audio/Galanthus_-_A_False_Hope.mp3",
+    "artwork": "img/portfolio/galanthus_selftitled.webp",
     "genresBroad": ["Rock"],
-    "genres": ["Classic Rock"]
+    "genres": ["Progressive Rock"]
+  },
+  {
+    "title": "Galanthus - Ainodecam",
+    "file": "audio/Galanthus_-_Ainodecam.mp3",
+    "artwork": "img/portfolio/galanthus_impact.webp",
+    "genresBroad": ["Rock"],
+    "genres": ["Progressive Rock"]
+  },
+  {
+    "title": "Galanthus - Big Dawn",
+    "file": "audio/Galanthus_-_Big_Dawn.mp3",
+    "artwork": "img/portfolio/galnthus3.webp",
+    "genresBroad": ["Rock"],
+    "genres": ["Progressive Rock"]
+  },
+  {
+    "title": "Galanthus - Challenge Beyond",
+    "file": "audio/Galanthus_-_Challenge_Beyond.mp3",
+    "artwork": "img/portfolio/galanthus_purple.webp",
+    "genresBroad": ["Rock"],
+    "genres": ["Progressive Rock"]
   }
 ];
 
